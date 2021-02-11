ea1b5ca921185ef87040d6d013ad4e7fb1322f8e
hd3379 <hd3379@naver.com>
2021-02-11 17:37:47 +0900

    Setting test World

---
+++ 쉐이더 작업공간/Practice/Assets/NewBehaviourScript.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+    public Camera followCamera;
+    void Start()
+    {
+    }
+    void Update()
+    {
+        Vector3 vec = new Vector3(
+            Input.GetAxisRaw("Horizontal"),
+            0,
+            Input.GetAxisRaw("Vertical"));
+        transform.Translate(vec);
+        Trun();
+    }
+
+    void Trun()
+    {
+        Ray ray = followCamera.ScreenPointToRay(Input.mousePosition);
+        RaycastHit rayHit;
+        if (Physics.Raycast(ray, out rayHit, 100))
+        {
+            Vector3 nextVec = rayHit.point - transform.position;
+            nextVec.y = 0;
+            transform.LookAt(transform.position + nextVec);
+        }
+    }
+}
