860256540666f2b4b4385aba6df39d8b7000e9c8
gwgames <pkw132@naver.com>
2021-02-19 19:32:14 +0900

    주인공불러오기,이동,주인공시야

---
+++ 스크립트 작업공간/MainFramework.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainFramework : MonoBehaviour
+{
+
+    private GameObject m_goMainPlayer;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //Prefabs 폴더에 있는 Player를 지정한 위치로 불러온다
+        m_goMainPlayer = GameObject.Instantiate(Resources.Load("Prefabs/Player"), new Vector3(-18, 2, -26), Quaternion.identity) as GameObject;
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ 스크립트 작업공간/SightControl.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+//vr기기 미연동시 시야체험을 위한 스크립트
+public class SightControl : MonoBehaviour
+{
+    //마우스 좌우값을 받아오는 문자열.
+    private const string AXIS_MOUSE_X = "Mouse X";
+
+    //마우스 상하값을 받아오는 문자열.
+    private const string AXIS_MOUSE_Y="Mouse Y";
+
+    //마우스 좌표 움직임을 멤버변수로 저장한다
+    private float m_fMouseX = 0f;//좌우
+
+    private float m_fMouseY = 0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        m_fMouseX = 0f;
+        m_fMouseY = 0f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+#if UNITY_EDITOR
+        m_fMouseX += Input.GetAxis(AXIS_MOUSE_X) * 1.0f;
+         //m_fMouseY -= Input.GetAxis(AXIS_MOUSE_Y) * 1.0f;
+        transform.transform.localRotation = Quaternion.Euler(m_fMouseY,m_fMouseX, 0);
+
+#endif
+    }
+}
