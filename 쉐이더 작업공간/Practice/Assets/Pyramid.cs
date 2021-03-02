b4da7c31cfb5826bdab044f153e791c831a5f188
hd3379 <hd3379@naver.com>
2021-03-03 02:52:13 +0900

    modify initPyramid skript

--- 쉐이더 작업공간/Practice/Assets/Pyramid.cs
+++ 쉐이더 작업공간/Practice/Assets/Pyramid.cs
@@ -4,21 +4,28 @@ using UnityEngine;
 
 public class Pyramid : MonoBehaviour
 {
-    public float moveSpeed = 3.0f;
-    public float RotateSpeed = 10.0f;
+    public float moveSpeed = 1.0f;
+    public float RotateSpeed = 200.0f;
     Vector3 RotationRate;
+    Vector3 TransRate;
     // Start is called before the first frame update
     void Start()
     {
-        int Ranx = Random.Range(-1, 1);
-        int Rany = Random.Range(-1, 1);
-        int Ranz = Random.Range(-1, 1);
+        float Ranx = Random.Range(-1.0f , 1.0f);
+        float Rany = Random.Range(-1.0f, 1.0f);
+        float Ranz = Random.Range(-1.0f, 1.0f);
         RotationRate = new Vector3(Ranx * RotateSpeed, Rany* RotateSpeed, Ranz * RotateSpeed);
+        Ranx = Random.Range(-1.0f, 1.0f);
+        Rany = Random.Range(-1.0f, 1.0f);
+        Ranz = Random.Range(-1.0f, 1.0f);
+        TransRate = new Vector3(Ranx * moveSpeed, Rany * moveSpeed, Ranz * moveSpeed);
     }
 
     // Update is called once per frame
     void Update()
     { 
+        transform.Translate(TransRate * Time.deltaTime);
         transform.Rotate(RotationRate * Time.deltaTime, Space.World) ;
     }
+
 }
--- 쉐이더 작업공간/Practice/Assets/initPyramids.cs
+++ 쉐이더 작업공간/Practice/Assets/initPyramids.cs
@@ -8,19 +8,22 @@ public class initPyramids : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        for (int i = 0; i < 10; i++)
-        {
-            Vector3 PyramidPosition = transform.position;
-            PyramidPosition.x += 3.0f * i;
-            PyramidPosition.y += 5.0f;
-            GameObject instance = Instantiate(Pyramid, PyramidPosition,
-                  transform.rotation) as GameObject;
-            //Destroy(instance, 2.0f);
-        }
     }
-
+    protected static float InitPyramid = 0;
     // Update is called once per frame
     void Update()
     {
+        InitPyramid += Time.deltaTime;
+        
+        while(InitPyramid > 0.001f)
+        { 
+            InitPyramid -= 0.001f;
+            Vector3 PyramidPosition = transform.position;
+            GameObject instance = Instantiate(Pyramid, PyramidPosition,
+                  transform.rotation);
+            Destroy(instance, 1.0f);
+        }
     }
+        
 }
+
