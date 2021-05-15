12d07015f0c3765d965195e5351ef45b7d783aac
gwgames <pkw132@naver.com>
2021-05-16 03:55:12 +0900

    시간출력

---
+++ 스크립트 작업공간/TIME.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TIME : MonoBehaviour
+{
+    public Text missiontext;
+    private float GameTime = 58;
+    private float min = 0;
+  
+    void Start()
+    {
+        
+    }
+
+ 
+    void Update()
+    {
+        GameTime += Time.deltaTime;
+       Debug.Log((int)GameTime);
+
+        if((int)GameTime == 60)
+        {
+           GameTime = 0;
+            min +=1;
+            GameTime += Time.deltaTime;
+            Debug.Log((int)GameTime);
+        }
+
+        if ((int)GameTime < 10)
+        {
+            missiontext.text = min + " : " + "0" + (int)GameTime;
+        }
+        else
+        {
+            missiontext.text = min + " : " + (int)GameTime;
+        }
+
+    }
+}
