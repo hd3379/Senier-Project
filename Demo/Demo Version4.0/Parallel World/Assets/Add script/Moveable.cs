5ace3409c08e797af1349df332660582810672b4
hd3379 <hd3379@naver.com>
2021-06-06 20:27:06 +0900

    add NavFunction

---
+++ Demo/Demo Version4.0/Parallel World/Assets/Add script/Moveable.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class Moveable : MonoBehaviour
+{
+    NavMeshAgent agent;
+
+    [SerializeField]
+    Transform target;
+
+    private void Awake()
+    {
+        agent = GetComponent<NavMeshAgent>();
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+            agent.SetDestination(target.position);
+        }
+    }
+}
