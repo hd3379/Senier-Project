f5dc62e331f697e419ffad5d2c42fe2a8a0c69c3
hd3379 <hd3379@naver.com>
2021-07-03 17:44:06 +0900

    ghost_randomMoveInWayPoints

---
+++ Demo/Demo Version5.0/Parallel World/Assets/Add script/MoveScript/Moveable_ghost.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class Moveable_ghost : MonoBehaviour
+{
+    NavMeshAgent agent;
+
+    [SerializeField]
+    public List<Transform> waypoints;
+    private Transform targetWaypoint;
+    private int targetWaypointIndex = 0;
+    private float minDistance = 0.5f;
+    private int lastWaypointIndex;
+
+    private void Awake()
+    {
+        agent = GetComponent<NavMeshAgent>();
+    }
+
+    void Start()
+    {
+        lastWaypointIndex = waypoints.Count - 1;
+        targetWaypoint = waypoints[targetWaypointIndex];
+        agent.SetDestination(targetWaypoint.position);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float distance = Vector3.Distance(transform.position, targetWaypoint.position);
+        CheckDistanceToWaypoint(distance);
+    }
+
+    void CheckDistanceToWaypoint(float currentDistance)
+    {
+        if(currentDistance <= minDistance)
+        {
+            targetWaypointIndex = Random.Range(0, lastWaypointIndex);
+            UpdateTargetWaypoint();
+        }
+    }
+
+    void UpdateTargetWaypoint()
+    {
+        if(targetWaypointIndex > lastWaypointIndex)
+        {
+            targetWaypointIndex = 0;
+        }
+        targetWaypoint = waypoints[targetWaypointIndex];
+        agent.SetDestination(targetWaypoint.position);
+    }
+}
